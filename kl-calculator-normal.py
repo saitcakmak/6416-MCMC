d9b220778dd3a080c2317bf2303992ebd150b954
Sait Cakmak <saitcakmak@outlook.com>
2019-04-11 17:37:52 -0400

    aaa

--- kl-calculator-normal.py
+++ kl-calculator-normal.py
@@ -1,17 +1,19 @@
 import numpy as np
 from scipy.stats import norm
+import matplotlib.pyplot as plt
 
 
 def get_input():
-    data = np.load("")
-    parameters = np.load("")
+    string = "10"
+    data = np.load("mcmc_norm_out_" + string + ".npy")
+    parameters = np.load("mcmc_norm_params_" + string + ".npy").item()
     mean, std_dev = parameters["post_mean"], np.sqrt(parameters["post_variance"])
     return data, mean, std_dev
 
 
 def create_hist(data):
     interval = [-5, 5]
-    bin_count = 10000
+    bin_count = 1000
     hist, bins = np.histogram(data, bin_count, interval, density=True)
     return hist, bins
 
@@ -28,6 +30,9 @@ def calc_div(hist, bins, mean, std_dev):
 
 def main():
     """do the job"""
+    data, mean, std_dev = get_input()
+    hist, bins = create_hist(data)
+    div = calc_div(hist, bins, mean, std_dev)
 
 
 if __name__ == "__main__":
