2cbabfd7f79928ec2ac0baa8c23cd2040d16ca38
Sait Cakmak <saitcakmak@outlook.com>
2019-04-14 15:11:30 -0400

    output

--- plotter.py
+++ plotter.py
@@ -7,27 +7,32 @@ def main():
                  "exp_10_30_0.09_wass_out.npy", "exp_10_30_0.25_wass_out.npy", "exp_10_30_0.0025_wass_out.npy",
                  "exp_10_30_1.0_wass_out.npy", "exp_10_30_4.0_wass_out.npy",
                  "exp_10_30_25.0_wass_out.npy"]
-
     data = {}
     for file in file_list:
         file_data = np.load("wass-dist/"+file).item()
         data[float(file[10:-13])] = {}
         for key in file_data.keys():
             data[float(file[10:-13])][key] = list(file_data[key].values())
-
     return data
 
 
 def plot_all(data):
     x_ax = [100, 500, 1000, 2000, 5000, 10000, 50000, 100000, 200000, 400000, 800000]
+    # create subplots with shared axis
     f, axarr = plt.subplots(int(len(data.keys())/2), 2, sharex=True, sharey=True)
+    # keys correspond to the covariances
     keys = sorted(data.keys())
+    # inner keys correspond to the burn-in length
     inner_keys = list(data[keys[0]].keys())
+    # clear up some of the data
     for i in [10, 50, 500, 2000, 20000]:
         inner_keys.remove(i)
+    # plot the subplots, each one corresponding to a covariance
     for i in range(len(keys)):
         for key in inner_keys:
+            # plot the corresponding line
             axarr[int(i/2), i % 2].plot(x_ax, data[keys[i]][key], label=key)
+            # fix out the plot details
             axarr[int(i/2), i % 2].set_ylim([0, 1])
             axarr[int(i/2), i % 2].set_title("Covariance = "+str(keys[i]), fontsize=7)
             axarr[int(i/2), i % 2].set_xlabel("Sample Size", fontsize=6)
